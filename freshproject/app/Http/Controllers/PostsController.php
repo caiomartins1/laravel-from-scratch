0fccd7090a0178c7f7afe8870c7a0e1019177ccc
caiomartins1 <dev.caiomartins@gmail.com>
2020-12-19 21:08:59 +0000

    Last lesson: Hello Eloquent

---
+++ freshproject/app/Http/Controllers/PostsController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use App\Models\Post;
+
+class PostsController
+{
+    public function show($slug)
+    {
+
+        $post = Post::where('slug', $slug)->firstOrFail();
+
+        return view('post', [
+            'post' => $post
+        ]);
+    }
+}
